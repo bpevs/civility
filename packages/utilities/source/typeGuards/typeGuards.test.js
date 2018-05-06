08d6cbb06814bbf022765f804925669eb657a415
ivebencrazy <ivebencrazy@users.noreply.github.com>
2018-05-05 17:29:41 -0700

    Updates test

--- packages/utilities/source/typeGuards/typeGuards.test.js
+++ packages/utilities/source/typeGuards/typeGuards.test.js
@@ -5,7 +5,7 @@ import * as typeGuards from "./typeGuards"
 const items = [
   "s", 0, 1, "1", Infinity, null, undefined, [], [ 1 ], {},
   { 0: 1, 1: 2 }, Number, new RegExp(/fsdf/), /fsdf/, 1.5234, Math.pow(2, 53),
-  new Error("hello"), NaN, new Date("1/1/1"), Promise.resolve(), new Set(), new Map(),
+  new Error("hello"), NaN, Date.UTC(1, 1, 1), Promise.resolve(), new Set(), new Map(),
 ];
 
 forEach(typeGuards, check => {
